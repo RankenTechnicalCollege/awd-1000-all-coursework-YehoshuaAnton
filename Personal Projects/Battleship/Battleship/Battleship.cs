fe094a9bc014376c5d660a2f9f556495500b3fd8
Yehoshua Anton <yehoshuaanton@gmail.com>
2026-09-18 11:18:31 -0500

    Began documentation of Battleship.cs

--- Personal Projects/Battleship/Battleship/Battleship.cs
+++ Personal Projects/Battleship/Battleship/Battleship.cs
@@ -1,9 +1,14 @@
 ﻿using System.Text;
 
-namespace Battleship
-{
-    internal class Battleship
-    {
+/*
+ * IDEAS FOR FUTURE IMPLEMENTATION:
+ *  - A second board
+ *  - A way for the player to manually place their ships
+ *  - Guessing algorithms for the computer
+ */
+
+namespace Battleship {
+    internal class Battleship {
         // Create boolean variables for the game
         static bool playGame = true, newGame = true;
         static int turnCount = 0, hitCount = 0;
@@ -12,96 +17,92 @@ namespace Battleship
         // Create variables for the board
         private const int BOARDSIZE = 10;
         static int letter, number;
-        static readonly string[,] playerBoard = new string[BOARDSIZE, BOARDSIZE], computerBoard = new string[BOARDSIZE, BOARDSIZE];
+        static readonly string[,] playerBoard = new string[BOARDSIZE, BOARDSIZE], computersBoard = new string[BOARDSIZE, BOARDSIZE];
 
-        static void Main(string[] args)
-        {
-            while (newGame == true)
-            {
+        /* THERE IS NO NEED FOR SO MANY CHECKS */
+        /* RECONFIGURE FOR POSITIVE CHECKS, NOT NEGATIVE */
+        static void Main(string[] args) {
+            // Run once at the beginning of a new game
+            while (newGame == true) {
+                // Reset all spaces of the board to " " and place the ships
                 SetBoard();
-                while (playGame == true)
-                {
+                // Run at the beginning of each turn
+                while (playGame == true) {
+                    // Erase the old board and redisplay the new one
                     DisplayBoard();
-                    // If the coordinates are correctly formatted and the player has not guessed them yet
-                    if (FindCoordinates() || playerBoard[letter, number] != " ")
-                    {
-                        turnCount--;    // Decrement the turn counter
-                    }
-                    else
-                    {        // If he hasn't yet guessed the coordinates
-                        // If the coordinates that were guessed are a miss
-                        if (computerBoard[letter, number] == null)
-                        {
-                            playerBoard[letter, number] = "*";  // Mark the corresponding space with a "*"
-                        }
-                        else
-                        {    // But if the guess was a hit
-                            if (playerBoard[letter, number] != " ")
-                            { // And if the space hasn't been guessed yet
-                                hitCount--;     // Decrement the hit counter
+                    // If the coordinates are not correctly formatted or the player has already guessed them...
+                    if (!FindCoordinates() || playerBoard[letter, number] != " ") {
+                        // Decrement the turn counter
+                        turnCount--;
+                      // If the player hasn't yet guessed the coordinates...
+                    } else {
+                        // If the coordinates that were guessed are a miss...
+                        if (computersBoard[letter, number] == null) {
+                            // Mark the corresponding space with a "*"
+                            playerBoard[letter, number] = "*";
+                          // But if the coordinates that were guessed are a hit...
+                        } else {
+                            // And if the space has already been guessed...
+                            if (playerBoard[letter, number] != " ") {
+                                // Decrement the hit counter
+                                hitCount--;
+                              // But if the coodrinates have not yet been guessed...
+                            } else {
+                                // Mark the corresponding space with a "!"
+                                playerBoard[letter, number] = "!";
+                                // Increment the hit counter
+                                hitCount++;
                             }
-                            else
-                            {
-                                playerBoard[letter, number] = "!";  // Mark the corresponding space with a "!"
-                                hitCount++;     // Increment the hit counter
-                            }
                         }
                     }
-                    turnCount++;    // Increment the turn counter
-                    EndGame();      // Run the end-of-game script
+                    // Increment the turn counter
+                    turnCount++;
+                    // Run the end-of-game script
+                    EndGame();
                 }
             }
         }
 
         // Clear the console and redisplay the board
-        static void DisplayBoard()
-        {
+        static void DisplayBoard() {
             Console.Clear();
-            Console.WriteLine($"Turn count: {turnCount}\nHit count: {hitCount}\n");
+            // For dev work only
+            //Console.WriteLine($"Turn count: {turnCount}\nHit count: {hitCount}\n");
             DrawGrid();
         }
 
         // Create the grid for the board
-        static void DrawGrid()
-        {
+        static void DrawGrid() {
             // Create the first line to show the numbers
             StringBuilder firstLine = new("   |");
-            for (int i = 1; i <= BOARDSIZE; i++)
-            {
+            for (int i = 1; i <= BOARDSIZE; i++) {
                 firstLine.Append(i < 10 ? $" {i} |" : $"{i} |");
             }
             Console.Write(firstLine.ToString());
             DrawLine();
             // For each row, write the corresponding uppercase letter at the start
-            for (int rows = 0; rows < playerBoard.GetLength(0); rows++)
-            {
+            for (int rows = 0; rows < playerBoard.GetLength(0); rows++) {
                 Console.Write($" {(char)('A' + (rows % 26))} |");
                 // For each column in the row, display what is in the space
-                for (int columns = 0; columns < playerBoard.GetLength(1); columns++)
-                {
+                for (int columns = 0; columns < playerBoard.GetLength(1); columns++) {
                     Console.Write($" {playerBoard[rows, columns]} |");
                 }
                 DrawLine();
             }
         }
 
-        static void DrawLine()
-        {
+        static void DrawLine() {
             Console.WriteLine();
-            for (int i = 0; i < playerBoard.GetLength(0) + 1; i++)
-            {
+            for (int i = 0; i < playerBoard.GetLength(0) + 1; i++) {
                 Console.Write("---+");
             }
             Console.WriteLine();
         }
 
         // Fill the not-yet-guessed spaces A-J and 1-10 with spaces
-        static void SetBoard()
-        {
-            for (int row = 0; row < playerBoard.GetLength(0); row++)
-            {
-                for (int column = 0; column < playerBoard.GetLength(1); column++)
-                {
+        static void SetBoard() {
+            for (int row = 0; row < playerBoard.GetLength(0); row++) {
+                for (int column = 0; column < playerBoard.GetLength(1); column++) {
                     playerBoard[row, column] = " ";
                 }
             }
@@ -109,8 +110,7 @@ namespace Battleship
         }
 
         // Algorithm for randomly placing ships at the beginning of the game
-        static void PlaceShips()
-        {
+        static void PlaceShips() {
             int horizontal, vertical;
             Random shipOrientation = new();
             Random shipPlacement = new();
@@ -124,55 +124,42 @@ namespace Battleship
                 new("Patrol Boat", 2)
             ];
 
-            foreach (Ship ship in ships)
-            {
+            foreach (Ship ship in ships) {
                 bool shipPlaced = false;
                 int orientation = shipOrientation.Next(2);
 
                 // Pick a random starting position for the ship, making sure it won't go over the side of the board
-                while (shipPlaced == false)
-                {
-                    if (orientation == 0)
-                    {
+                while (shipPlaced == false) {
+                    if (orientation == 0) {
                         horizontal = shipPlacement.Next((BOARDSIZE + 1) - ship.Length);
                         vertical = shipPlacement.Next(BOARDSIZE);
                     }
-                    else
-                    {
+                    else {
                         horizontal = shipPlacement.Next(BOARDSIZE);
                         vertical = shipPlacement.Next((BOARDSIZE + 1) - ship.Length);
                     }
 
                     // Loop through the spaces the ship is trying to occupy and make sure they are all empty
-                    for (int shipPosition = 0; shipPosition < ship.Length; shipPosition++)
-                    {
-                        if (orientation == 0)
-                        {
-                            if (computerBoard[horizontal + shipPosition, vertical] != null)
-                            {
+                    for (int shipPosition = 0; shipPosition < ship.Length; shipPosition++) {
+                        if (orientation == 0) {
+                            if (computersBoard[horizontal + shipPosition, vertical] != null) {
                                 break;
                             }
                         }
-                        else
-                        {
-                            if (computerBoard[horizontal, vertical + shipPosition] != null)
-                            {
+                        else {
+                            if (computersBoard[horizontal, vertical + shipPosition] != null) {
                                 break;
                             }
                         }
 
                         // If there are no more spaces to check, loop through the ship's length and place it on the computer's board
-                        if (shipPosition == ship.Length - 1)
-                        {
-                            for (int i = 0; i < ship.Length; i++)
-                            {
-                                if (orientation == 0)
-                                {
-                                    computerBoard[horizontal + i, vertical] = Convert.ToString(ship.Name[0]);
+                        if (shipPosition == ship.Length - 1) {
+                            for (int i = 0; i < ship.Length; i++) {
+                                if (orientation == 0) {
+                                    computersBoard[horizontal + i, vertical] = Convert.ToString(ship.Name[0]);
                                 }
-                                else
-                                {
-                                    computerBoard[horizontal, vertical + i] = Convert.ToString(ship.Name[0]);
+                                else {
+                                    computersBoard[horizontal, vertical + i] = Convert.ToString(ship.Name[0]);
                                 }
                             }
                             shipPlaced = true;
@@ -183,74 +170,83 @@ namespace Battleship
         }
 
         // Create a method to ensure all guesses are correctly formatted
-        static bool FindCoordinates()
-        {
+        static bool FindCoordinates() {
             Console.Write("Input a letter then a number (e.g. A1): ");
             string coordinates = Console.ReadLine().ToUpper();
             if (coordinates.Trim().Length < 2 ||        // Check if the guess has both a letter and number
                (((int)coordinates[0] < 'A') || ((int)coordinates[0] > 'A' + (BOARDSIZE - 1))) || // The letter is between A and J
-               (((int)coordinates[1] < '1') || ((int)coordinates[1] > '9')))
-            { // And the number is between 1 and 10
+               (((int)coordinates[1] < '1') || ((int)coordinates[1] > '9'))) { // And the number is between 1 and 10
                 Console.WriteLine("Please input your coordinates correctly");
                 Thread.Sleep(1000);
-                return true;
+                return false;
             }
-            else
-            {            // If it does
+            else {            // If it does
                 letter = (int)coordinates[0] - 'A';
                 number = (int)coordinates[1] - '1';
 
+                /* LAZY CODING - CHANGE IT */
+                /* Find a way to work a range operator in: [1..^0] - from the second index to the last */
                 // If the guess's length is more than two, set the number portion of it to 10
-                if (coordinates.Length > 2)
-                {
+                if (coordinates.Length > 2) {
                     number = 9;
                 }
-
-                return false;
+                return true;
             }
 
         }
 
-        // Create a method to end the game
-        static void EndGame()
-        {
-            if (hitCount == HITCOUNT || turnCount == TURNCOUNT)
-            {
+        // Create a method to check for the end of the game
+        static void EndGame() {
+            // If either the hitcount reached the limit (win), or the turncount hit the limit (loss)...
+            if (hitCount == HITCOUNT || turnCount == TURNCOUNT) {
+                // Show the winning board
                 DisplayBoard();
+                // Ask if the player wants to play again
                 NewGame();
             }
         }
 
         // Create a method to see if the player wants to play again
-        static void NewGame()
-        {
+        static void NewGame() {
+            // Pause for 1 second for effect
             Thread.Sleep(1000);
+            // Stop the game by setting the turn loop to false
             playGame = false;
+            // Display a message asking if the player wants to play again
             Console.WriteLine("Play again? Y/N");
+            // Parse their input - only the first letter matters
             string playAgain = Console.ReadLine().ToUpper();
-            if (playAgain[0] == 'Y')
-            {
+            // If the parsed input is 'Y'...
+            if (playAgain[0] == 'Y') {
+                // Restart the gameplay loop by setting playGame to true
                 playGame = true;
+                // Reset the turncount and hitcount back to 0
                 turnCount = 0;
                 hitCount = 0;
+                // Call the method to once again set all the spaces on the board to " " and randomize ship placement
                 SetBoard();
-            }
-            else if (playAgain[0] == 'N')
-            {
+              // If the parsed input is 'N'...
+            } else if (playAgain[0] == 'N') {
+                // End the program by setting the "game is running" loop to false
                 newGame = false;
-            }
-            else
-            {
+                // Close the console window
+                Environment.Exit(0);
+              // If the parsed input is neither 'Y' nor 'N'...
+            } else {
+                // Display a message asking the player to provide valid input
                 Console.WriteLine("Please input either \"Y\" or \"N\"");
+                //Pause for 1 second for effect
                 Thread.Sleep(1000);
-                EndGame();
+                // Redisplay the console
+                DisplayBoard();
+                // Run this method again
+                NewGame();
             }
         }
     }
 
-    // Create a class for the ships
-    internal class Ship(string shipName, int shipLength)
-    {
+    // Create a class for the ships and a constructor for the Ship class
+    internal class Ship(string shipName, int shipLength) {
         public string Name { get; set; } = shipName;
         public int Length { get; set; } = shipLength;
     }
